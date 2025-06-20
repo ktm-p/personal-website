13530f86e1f022069e76e3bf1a6af19d37781971
ktm-p <ktmichael,pham@gmail.com>
2025-06-19 22:39:10 -0700

    Fixed tab name not changing for blog posts. New blog!

---
+++ .history/src/routes/+layout_20250618231222.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Head from "$lib/components/Head.svelte";
+    import Nav from "$lib/components/Nav.svelte";
+    import "$lib/styles/style.css";
+    import { onMount } from "svelte";
+    import { page } from "$app/stores";
+
+    // Dynamic title changing
+    const websiteName = "ktm-p";
+    $: routeTitle = $page.data?.title || null;
+    $: directory = $page.url.pathname.split("/").slice(1, 2).filter(Boolean);
+    $: tabName = directory.length ? directory.map(str => str.charAt(0).toUpperCase() + str.slice(1))
+    : ["Home"];
+    $: title = [routeTitle || tabName.join(" "), websiteName].join(" | ");</script>
+
+<Head />
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<Nav />
+
+<main>
+    <slot />
+</main>
