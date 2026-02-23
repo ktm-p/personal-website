98aae59f1afc933697691e5fba08cdb63bd6877e
ktm-p <ktmichael.pham@gmail.com>
2026-02-22 19:45:24 -0800

    Rasterizer Project.

---
+++ .history/src/routes/api/posts/+server_20260222165938.js
@@ -0,0 +1,14 @@
+import { fetchMarkdownPosts } from '$lib/utils';
+import { json } from '@sveltejs/kit';
+
+export const GET = async () => {
+	const allPosts = await fetchMarkdownPosts();
+
+	const sortedPosts = allPosts.sort((a, b) => {
+		return new Date(b.meta.date) - new Date(a.meta.date);
+	});
+
+	console.log(json(sortedPosts));
+
+	return json(sortedPosts);
+};
