6e179cb087ca547b03d99039c8e79777c3d837a6
ktm-p <ktmichael,pham@gmail.com>
2026-02-22 19:45:24 -0800

    Rasterizer Project.

---
+++ src/.history/routes/courses/+page_20250802162356.svelte
@@ -0,0 +1,78 @@
+<section class="course-timeline">
+    <h1 class="section-head">Courses</h1>
+    <div class="course-container" id="sp25">
+        <h2 class="semester">Spring 2025</h2>
+        <ul class="courses">
+                    <li><a class="class-name" href="https://www.avishaytal.org/cs-172-computability-and-complexity" target="_blank"> COMPSCI 172</a>:  Computability and Complexity</li>
+            <li><a class="class-name" href="https://eecs189.org/" target="_blank"> COMPSCI 189</a>:  Introduction to Machine Learning</li>
+            <li><a class="class-name" href="https://data140.org/" target="_blank"> DATA C140</a>: Probability for Data Science</li>
+            <li><a class="class-name" href="http://persson.berkeley.edu/math124/" target="_blank"> MATH 124</a>: Introduction to Analysis</li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="sp25">
+        <h2 class="semester">Spring 2025</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://cs168.org/" target="_blank"> COMPSCI 168</a>:  Introduction to the Internet</li>
+            <li><a class="class-name" href="https://cs170.org/" target="_blank"> COMPSCI 170</a>: Efficient Algorithms and Intractable Problems</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-104" target="_blank"> MATH 104</a>: Introduction to Analysis</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-116" target="_blank"> MATH 116</a>: Cryptography </li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="fa24">
+        <h2 class="semester">Fall 2024</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://cs162.org/" target="_blank"> COMPSCI 162</a>:  Operating Systems and System Programming</li>
+            <li><a class="class-name" href="https://cdss.berkeley.edu/data-104-human-contexts-and-ethics-data" target="_blank"> DATA C104</a>: Human Contexts and Ethics of Data</li>
+            <li><a class="class-name" href="https://www2.eecs.berkeley.edu/Courses/EECS127/" target="_blank"> EECS 127</a>: Optimization Models in Engineering</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-113" target="_blank"> MATH 113</a>: Introduction to Abstract Algebra </li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="su24">
+        <h2 class="semester">Summer 2024</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="http://persson.berkeley.edu/math128a/" target="_blank"> MATH 128A</a>: Numerical Analysis </li>
+            <li><a class="class-name" href="https://philosophy.berkeley.edu/courses/detail/1680" target="_blank"> PHILOS 12A</a>: Introduction to Logic</li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="sp24">
+        <h2 class="semester">Spring 2024</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://sp24.cs161.org/" target="_blank"> COMPSCI 161</a>: Computer Security</li>
+            <li><a class="class-name" href="https://inst.eecs.berkeley.edu/~cs188/sp24/" target="_blank"> COMPSCI 188</a>: Introduction to Artificial Intelligence</li>
+            <li><a class="class-name" href="https://ds100.org/sp24/" target="_blank"> DATA C100</a>: Principles and Techniques of Data Science</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-135" target="_blank"> MATH 135</a>: Introduction to Set Theory</li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="fa23">
+        <h2 class="semester">Fall 2023</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://inst.eecs.berkeley.edu/~cs61c/fa23/" target="_blank"> COMPSCI 61C</a>: Great Ideas of Computer Architecture (Machine Structures)</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/linguis-100" target="_blank"> LINGUIS 100</a>: Introduction to Linguistic Science </li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-110" target="_blank"> MATH 110</a>: Abstract Linear Algebra</li>
+        </ul>
+    </div>
+
+    <div class="course-container" id="sp23">
+        <h2 class="semester">Spring 2023</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://sp23.datastructur.es/" target="_blank"> COMPSCI 61B</a>: Data Structures</li>
+            <li><a class="class-name" href="https://sp23.eecs70.org/" target="_blank"> COMPSCI 70</a>: Discrete Mathematics and Probability Theory</li>
+            <li><a class="class-name" href="https://www2.eecs.berkeley.edu/Courses/CS370/" target="_blank"> COMPSCI 370</a>: Adaptive Instruction Methods in Computer Science</li>
+
+        </ul>
+    </div>
+
+    <div class="course-container" id="fa22">
+        <h2 class="semester">Fall 2022</h2>
+        <ul class="courses">
+            <li><a class="class-name" href="https://inst.eecs.berkeley.edu/~cs61a/fa22/" target="_blank"> COMPSCI 61A</a>: Structure and Interpretation of Computer Programs</li>
+            <li><a class="class-name" href="http://www.data8.org/fa22/" target="_blank"> DATA C8</a>: Foundations of Data Science</li>
+            <li><a class="class-name" href="https://classes.berkeley.edu/content/math-54" target="_blank"> MATH 54</a>: Linear Algebra and Differential Equations</li>
+        </ul>
+    </div>
+</section>
