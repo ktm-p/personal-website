64c5312e89cee4b36bf5feae093f085051112887
ktm-p <ktmichael.pham@gmail.com>
2026-04-24 02:42:35 -0700

    Updated .gitignore.

--- .history/src/routes/blog/[slug]/+page_20250415124104.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let data;
-</script>
-
-<article>
-	<h1 class="post-title">{data.title}</h1>
-	<p class="post-date">Published: {data.date}</p>
-	<svelte:component this={data.content} />
-</article>
