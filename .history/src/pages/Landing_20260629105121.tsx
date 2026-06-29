a10b92d0c3de8e457af4d283737dc07bc713aae4
ktm-p <ktmichael.pham@gmail.com>
2026-06-29 13:09:46 -0700

    feat: rewrite using react

--- .history/src/pages/Landing_20260629105121.tsx
+++
@@ -1,7 +0,0 @@
-export default function Landing() {
-    return (
-        <main className="landing">
-            <h1>Michael Pham</h1>
-        </main>
-    )
-}
--- .history/src/pages/Landing_20260629114627.tsx
+++
@@ -1,8 +0,0 @@
-export default function Landing() {
-    return (
-        <main className="landing">
-            <h1>Michael Pham</h1>
-            <h2>Software Engineer</h2>
-        </main>
-    )
-}
